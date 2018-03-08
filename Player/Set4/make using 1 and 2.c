58e76b4de0cbcfff9f3426a45c0678c5f0f6cb80
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-03-08 16:32:41 +0530

    Update make using 1 and 2.c

--- Player/Set4/make using 1 and 2.c
+++ Player/Set4/make using 1 and 2.c
@@ -2,10 +2,22 @@
 #include<conio.h>
 void main()
 {
-int n;
+int n,a,b,c=0,i;
 clrscr();
 printf("Enter the number:");
 scanf("%d",&n);
-printf("The no. of ways: %d",(n/2)+1);
+for(i=0;i<=n/2;i++)
+{
+a=i*2;
+if(a<=n)
+{
+c+=1;
+}
+else
+{
+break;
+}
+}
+printf("Ways : %d",c);
 getch();
 }
